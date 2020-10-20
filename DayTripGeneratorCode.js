b6d83c5923e84045c3ab2fac9b4f81ff38824b27
xxiong93 <xengxiong93@gmail.com>
2020-10-19 23:26:47 -0500

    Added in a possible condition for testing whether the user will accept the selected options or opt out to re-select new ones **incomplete**

--- DayTripGeneratorCode.js
+++ DayTripGeneratorCode.js
@@ -6,24 +6,24 @@
 // i need to choose one location out of the possible options
 // if a solution is selected, print it to console
     //break out of loop after a selection is made
-
+let trip = [];
 let destination = ["Sheboygan", "Milwaukee", "Wausau"];
 
 function randomDestination(){
 
     for(let i = Math.floor(Math.random() * 3); i < destination.length; i++){
         let choice = i;
-        
+    
         if(choice === 0 || choice === 1 || choice === 2){
             console.log(destination[i]);
             break;
 
         }
-        
+             
     }
-
+    
 }
-randomDestination();
+
 
 
 
@@ -45,7 +45,7 @@ function randomRestaurant(){
 
     }
 }
-randomRestaurant();
+
 
 
 //RANDOM MODE OF TRANSPORTATION
@@ -63,9 +63,9 @@ function randomTransport(){
             break;
         }
     }
-
+    
 }
-randomTransport();
+
 
 
 //RANDOM ENTERTAINMENT
@@ -79,13 +79,40 @@ function randomEntertainment(){
         let choice = i;
 
         if(choice === 0 || choice === 1 || choice === 2 || choice === 3){
-            console.log(entertainment[i]);
+            console.log(entertainment[i]);          
             break;
         }
     }
-
+    
 
 }
 
-randomEntertainment();
+let destinationChoice = randomDestination();
+let restaurantChoice = randomRestaurant();
+let transportChoice = randomTransport();
+let entertainmentChoice = randomEntertainment();
+let question = prompt("Do you want to re-select? Y/N for an answer");
+if(question === "y" || question === "Y"){
+    console.log("Here is your day trip intinerary: " + "Your vehicle for transportation is a " +
+    transportChoice + "." + "/n" + "You will be going to " + destinationChoice + "." + "/n" + 
+    "You will be eating at " + restaurantChoice + " followed by " + entertainmentChoice + ".");
+}
+
+
+
+
+// RE-SELECT 
+// as if user likes the selected options
+// if user likes selections, store options
+    // otherwise re-select the option(s)
+        // re-run the function if they choose option 1-4 for destination, restaurant, transport, or entertainment
+        // store new option generated
+
+
+// let confirm = prompt("Do you want to re-select any options? (Enter Y/y for yes and N/n for no)")        
+// if(confirm === "Y" || confirm === 'y'){
+//     console.log(randomEntertainment());
+
+// }
+
 
