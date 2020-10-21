dd2f12adb51219a12012f7892288b2aaa78e6306
xxiong93 <xengxiong93@gmail.com>
2020-10-20 19:40:40 -0500

    Added a function that will call the random functions for the day trip options

--- DayTripGeneratorCode.js
+++ DayTripGeneratorCode.js
@@ -6,15 +6,17 @@
 // i need to choose one location out of the possible options
 // if a solution is selected, print it to console
     //break out of loop after a selection is made
-let trip = [];
-let destination = ["Sheboygan", "Milwaukee", "Wausau"];
+
+
+
 
 function randomDestination(){
 
+    let destination = ["Sheboygan", "Milwaukee", "Wausau"];
+
     for(let i = Math.floor(Math.random() * 3); i < destination.length; i++){
-        let choice = i;
-    
-        if(choice === 0 || choice === 1 || choice === 2){
+          
+        if(i === 0 ||  i  === 1 || i === 2){
             console.log(destination[i]);
             break;
 
@@ -26,18 +28,13 @@ function randomDestination(){
 
 
 
-
-//RANDOM RESTAURANT
-
-let restaurant = ["Legend Larry's", "Pho Viet", "World Market"];
-
 function randomRestaurant(){
+
+    let restaurant = ["Legend Larry's", "Pho Viet", "World Market"];
 
     for(let i = Math.floor(Math.random() * 3); i < restaurant.length; i++){
 
-        let choice = i;
-
-        if(choice === 0 || choice === 1 || choice === 2){
+        if(i === 0 ||  i  === 1 || i === 2){
             console.log(restaurant[i]);
             break;
 
@@ -47,18 +44,13 @@ function randomRestaurant(){
 }
 
 
-
-//RANDOM MODE OF TRANSPORTATION
-
-let transportation = ["Ferrari 458", "Lambo Huracan", "Focus ST", "Godzilla"];
-
 function randomTransport(){
 
+    let transportation = ["Ferrari 458", "Lambo Huracan", "Focus ST", "Godzilla"];
+
     for(let i = Math.floor(Math.random()* 4); i < transportation.length; i++){
-
-        let choice = i;
-
-        if(choice === 0 || choice === 1 || choice === 2 || choice === 3){
+        
+        if(i === 0 ||  i  === 1 || i === 2 || i === 3){
             console.log(transportation[i]);
             break;
         }
@@ -67,36 +59,48 @@ function randomTransport(){
 }
 
 
-
-//RANDOM ENTERTAINMENT
-
-let entertainment = ["Museum Tour!", "Zoo day!", "Beach and bikes!", "Explore a park"];
-
 function randomEntertainment(){
+
+    let entertainment = ["Museum Tour!", "Zoo day!", "Beach and bikes!", "Explore a park"];
 
     for(let i = Math.floor(Math.random() * 4); i < entertainment.length; i++){
 
-        let choice = i;
-
-        if(choice === 0 || choice === 1 || choice === 2 || choice === 3){
+        if(i === 0 ||  i  === 1 || i === 2 || i === 3){
             console.log(entertainment[i]);          
             break;
         }
     }
+     
+
+}
+
+function dayTrip(){
+
     
+    // loop here
+    //while(false){
+    let destinationChoice = randomDestination();
+    let restaurantChoice = randomRestaurant();
+    let transportChoice = randomTransport();
+    let entertainmentChoice = randomEntertainment();
+    // write out current trip
+    // ask for confirmation of curent trip, if no confirmation, go again
+    console.log("You will be going to " + destinationChoice + " in a " + 
+    transportChoice + ". You will be doing this activity, " +  entertainmentChoice + 
+    ", and eating at " + restaurantChoice + ".");
+    //}
 
 }
+dayTrip();
+// cal dayTrip here
 
-let destinationChoice = randomDestination();
-let restaurantChoice = randomRestaurant();
-let transportChoice = randomTransport();
-let entertainmentChoice = randomEntertainment();
-let question = prompt("Do you want to re-select? Y/N for an answer");
-if(question === "y" || question === "Y"){
-    console.log("Here is your day trip intinerary: " + "Your vehicle for transportation is a " +
-    transportChoice + "." + "/n" + "You will be going to " + destinationChoice + "." + "/n" + 
-    "You will be eating at " + restaurantChoice + " followed by " + entertainmentChoice + ".");
-}
+// let question = prompt("Do you want to re-select? Y/N for an answer");
+// if(question === "y" || question === "Y"){
+//     console.log("Here is your day trip intinerary: " + "Your vehicle for transportation is a " +
+//     transportChoice + "." + "You will be going to " + destinationChoice + "." + 
+//     " You will be eating at " + restaurantChoice + " followed by " + entertainmentChoice + ".");
+// }
+
 
 
 
@@ -109,10 +113,5 @@ if(question === "y" || question === "Y"){
         // store new option generated
 
 
-// let confirm = prompt("Do you want to re-select any options? (Enter Y/y for yes and N/n for no)")        
-// if(confirm === "Y" || confirm === 'y'){
-//     console.log(randomEntertainment());
-
-// }
 
 
